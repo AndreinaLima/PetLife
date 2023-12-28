019aa90af649e6e4487b97ce21bbb331051faca9
Andreina Lima <andreinaadelima14@gmail.com>
2023-12-28 18:37:30 -0300

    site-2

---
+++ index.js
@@ -0,0 +1,10 @@
+var elementosDuvida = document.querySelectorAll('.duvida')
+
+elementosDuvida.forEach(function (duvida) {
+duvida.addEventListener('click', function () {
+ duvida.classList.toggle('ativa')
+})
+})
+function nome(argumento) {
+
+}
